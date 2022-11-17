54e73816ab3d286c07061da48308d2d3bf44ad6f
p4rty4nimAl <p4rty4nimAl@users.noreply.github.com>
2022-11-17 22:30:55 +0000

    new build!

--- build/route-docs.chunk.7a1ce.esm.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"5+aR":function(o,n,s){"use strict";s.r(n),function(o){var e=s("hosL"),t=s("9ZoI");n.default=class extends e.Component{render(){return o("div",{class:t.a.home},o("p",null,"docs"))}}}.call(this,s("hosL").h)},"9ZoI":function(o,n){"use strict";n.a={home:"home__TjhaP",bg:"bg__Po6T5"}}}]);
-//# sourceMappingURL=route-docs.chunk.7a1ce.esm.js.map
