0417b061abdd25443760c1e36de79926961fb635
p4rty4nimAl <p4rty4nimAl@users.noreply.github.com>
2023-03-12 20:35:59 +0000

    Whoops :)

--- src/index.js
+++ src/index.js
@@ -13,31 +13,6 @@ import Portfolio from "./routes/portfolio";
 import Docs from "./routes/docs";
 import Error from './routes/error';
 
-class App extends Component {
-    render() {
-        return (
-            <div id="app">
-                <Header />
-                <Router>
-                    <Home path="/" />
-                    <Projects path="/projects/" />
-                        <Pinger path="/projects/mcserverping" />
-                        <Ciphers.Weezd path='/projects/0090C4/' />
-                        <Ciphers.Bipher path='/projects/bipher/' />                    
-                        <Ciphers.CaesarVariation path='/projects/caesarVariation/' />
-                        <Ciphers.CRC path='/projects/continuousRegisterCipher/' />
-                        <Ciphers.HashKeyCipher path='/projects/hashKeyCipher/' />
-                        <Ciphers.KCRC path='/projects/keyedCRC/' />
-                        <Ciphers.HKCRCR path='/projects/hCRCRework/' />
-                        <Ciphers.Weezd2 path='/projects/K0090C4/' />
-                        <Ciphers.Keysmash path='/projects/keysmashCipher/' />
-                        <Poems.Index path='/projects/poetry/' />
-                            <Poems.Igno path='/projects/poetry/Ignorance/' />
-                            <Poems.Smep path='/projects/poetry/someoneElsesPanopticon/' />
-                            <Poems.Yanwe path='/projects/poetry/nothingWithoutMe/' />
-                            <Poems.Mnop path='/projects/poetry/moonlitOverpass/' />
-                    <Portfolio path="/portfolio/" />
-                    <Docs path="/docs/" />
 const App = () => {
     return (
         <div id="app">
