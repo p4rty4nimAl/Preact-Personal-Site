1aa0756e8b075673cd538d15749258a5afa26072
actuallyp4@gmail.com <p4rty4nimAl@users.noreply.github.com>
2023-05-27 12:01:03 +0100

    update config + robots.txt

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,16 @@
-export default (config, env, helpers) => {
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import { resolve } from 'path';
+
+export default (config, env) => {
     if (env.isProd) {
         config.devtool = false;
     }
+    config.plugins.push(
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: '*',
+                context: resolve(__dirname, 'src/assets')
+            }],
+        }),
+    );
 }
