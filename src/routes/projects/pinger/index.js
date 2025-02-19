d71e6fde996dc95fb8070e07eb58f3569aeac3b7
p4rty4nimAl <69524746+p4rty4nimAl@users.noreply.github.com>
2025-02-19 01:38:38 +0000

    Transition MCSP to own API

--- src/routes/projects/pinger/index.js
+++ src/routes/projects/pinger/index.js
@@ -20,14 +20,14 @@ const Pinger = () => {
     const gather = () => {
         // 25565 == Minecraft: JE default port
         const [address, port = 25565] = rawAddress.split(":");
-        request.open("GET", `https://mcapi.us/server/status?ip=${address}&port=${port}`);
+        request.open("GET", `https://api.xtraea.com/v0/minecraft_server_status?ip=${address}&port=${port}`);
         request.send();
     }
 
     return (
         <span class={style.pinger}>
             <p>Enter the server's address (and port, if applicable): </p><br />
-            <input onChange={((event) => setAddress(event.target.value))} type="text" /><br />
+            <input onChange={event => setAddress(event.target.value)} type="text" /><br />
             <button onclick={gather}>Click to ping the server for information!</button><br />
             <p>The results will be displayed below: </p>
             <DisplayOutput output={output} playerList={playerList} />
@@ -40,6 +40,7 @@ const DisplayOutput = (props) => {
     const {output, playerList} = props;
     let linkArray = [];
         for (let i = 0; i < playerList?.length; i++) {
+            // drop entries injected by server software to advertise
             if (playerList[i].includes("§")) continue;
             linkArray.push(
                 <>
